64d1dd4093b93d9a49ccaa1c22966637750a07fe
BenjaminSchedl <122736932+BenjaminSchedl@users.noreply.github.com>
2024-07-15 17:56:53 +0200

    Update sign.py

--- sign.py
+++ sign.py
@@ -10,7 +10,7 @@ def sign(m):
     account = w3.eth.account.create()
 
     eth_address = account.address
-    private_key = account.privateKey
+    private_key = account.key
 
     message = encode_defunct(text=m)
 
