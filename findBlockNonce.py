e9a5d6a075668cd32d610e34e6eec4c1dc905d92
BenjaminSchedl <122736932+BenjaminSchedl@users.noreply.github.com>
2024-07-22 20:05:17 +0200

    Update findBlockNonce.py

--- findBlockNonce.py
+++ findBlockNonce.py
@@ -18,8 +18,24 @@ def mine_block(k, prev_hash, rand_lines):
         print("mine_block expects positive integer")
         return b'\x00'
 
-    # TODO your code to find a nonce here
+    transactions = ''.join(rand_lines).encode('utf-8')
+    
+	nonce = 0
+    while True:
+		nonce_bytes = str(nonce).encode('utf-8')
+		hasher = hashlib.sha256()
+        hasher.update(prev_hash)
+        hasher.update(transactions)
+        hasher.update(nonce_bytes)
+        result_hash = hasher.hexdigest()
 
+		binary_hash = bin(int(result_hash, 16))[2:].zfill(256)
+
+		if binary_hash.endswith('0' * k):
+            break
+        
+        nonce += 1
+    
     assert isinstance(nonce, bytes), 'nonce should be of type bytes'
     return nonce
 
