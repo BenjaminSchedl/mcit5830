6d96c10b759d188f1db194048880552a79f4c417
BenjaminSchedl <122736932+BenjaminSchedl@users.noreply.github.com>
2024-07-31 20:59:14 +0200

    Source_first_commit

--- Source.sol
+++ Source.sol
@@ -22,15 +22,32 @@ contract Source is AccessControl {
     }
 
 	function deposit(address _token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+        require(approved[_token], "Token not registered");
+
+        bool success = ERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        require(success, "Transfer failed");
+
+        emit Deposit(_token, _recipient, _amount);
 	}
 
 	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
-		//YOUR CODE HERE
+        require(hasRole(WARDEN_ROLE, msg.sender), "Caller is not a warden");
+
+        bool success = ERC20(_token).transfer(_recipient, _amount);
+        require(success, "Transfer failed");
+
+        emit Withdrawal(_token, _recipient, _amount);
 	}
 
 	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
-		//YOUR CODE HERE
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
+
+        require(!approved[_token], "Token already registered");
+
+        approved[_token] = true;
+        tokens.push(_token);
+
+        emit Registration(_token);
 	}
 
 
